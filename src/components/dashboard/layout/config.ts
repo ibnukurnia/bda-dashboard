c20c490530bf39c95c5c407945e9a08d766d0b70
Muhammad Rafi Karim <rafi.karim@sharingvision.biz>
2024-07-19 15:43:04 +0700

    Masih Resolved merge conflicts

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -15,17 +15,3 @@ export const navItems: NavItemConfig[] = [
 export const logoutItem: NavItemConfig = { key: 'logout', href: paths.auth.signIn, icon: 'log-out' };
 
 // the nav config
-
-import type { NavItemConfig } from '@/types/nav';
-import { paths } from '@/paths';
-
-export const navItems: NavItemConfig[] = [
-  { key: 'overview', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'anomaly-prediction', href: paths.dashboard.anomaly, icon: 'radio' },
-  { key: 'situation', href: paths.dashboard.situation, icon: 'database' },
-  // { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  // { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
-  // { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
-];
-
-export const logoutItem: NavItemConfig = { key: 'logout', href: paths.auth.signIn, icon: 'log-out' };
