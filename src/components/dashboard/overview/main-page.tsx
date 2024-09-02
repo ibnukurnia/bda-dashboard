4d75310006b13f5953607cae23f1eadafdcdae64
Rizky Akbar <rizkyakhid@gmail.com>
2024-09-02 14:28:03 +0700

    feature: revamp halaman overview #3

--- src/components/dashboard/overview/main-page.tsx
+++ src/components/dashboard/overview/main-page.tsx
@@ -105,114 +105,6 @@ const sourceData = [
 
 const dates = ['Jan 21', 'Jan 22', 'Jan 23', 'Jan 24', 'Jan 25', 'Jan 26', 'Jan 27']
 
-const FilterPanel = ({
-  onSelectServices,
-}: {
-  onSelectServices: (services: { name: string; data: number[] }[]) => void
-}) => {
-  const [selectedSourceIndices, setSelectedSourceIndices] = useState<number[]>([])
-  const [selectedServiceOptions, setSelectedServiceOptions] = useState<string[]>([])
-
-  const handleSourceClick = (index: number) => {
-    setSelectedSourceIndices((prevIndices) =>
-      prevIndices.includes(index) ? prevIndices.filter((i) => i !== index) : [...prevIndices, index]
-    )
-  }
-
-  const handleServiceChange = (service: { name: string; data: number[] }) => {
-    setSelectedServiceOptions((prevServices) =>
-      prevServices.includes(service.name)
-        ? prevServices.filter((s) => s !== service.name)
-        : [...prevServices, service.name]
-    )
-
-    // Update the selected services
-    onSelectServices((prevServices: { name: string; data: number[] }[]) => {
-      const serviceExists = prevServices.some((s) => s.name === service.name)
-
-      // Calculate the new array of services
-      const updatedServices = serviceExists
-        ? prevServices.filter((s) => s.name !== service.name)
-        : [...prevServices, service]
-
-      // Directly pass the updated array to onSelectServices
-      return updatedServices
-    })
-  }
-
-  const selectedServices = selectedSourceIndices
-    .flatMap((index) =>
-      sourceData[index].services.map((service) => ({
-        ...service,
-        name: `${sourceData[index].name} - ${service.name}`, // Append source name to service name
-      }))
-    )
-    .reduce(
-      (acc, service) => {
-        const existingService = acc.find((s) => s.name === service.name)
-        if (existingService) {
-          existingService.count += service.count
-        } else {
-          acc.push({ ...service })
-        }
-        return acc
-      },
-      [] as { name: string; count: number; data: number[] }[]
-    )
-
-  return (
-    <div className="bg-gray-900 text-white w-64 p-4 flex flex-col gap-6">
-      <div className="flex flex-col gap-3">
-        <h3 className="text-lg font-bold">Source Data</h3>
-        <ul className="flex flex-col gap-2">
-          {sourceData.map((item, index) => (
-            <li key={index} className={`flex items-center gap-1 cursor-pointer m-0`}>
-              <input
-                type="checkbox"
-                value={item.name}
-                checked={selectedSourceIndices.includes(index)}
-                onChange={() => handleSourceClick(index)}
-                className="mr-2 cursor-pointer py-1"
-              />
-              <span className="" onClick={() => handleSourceClick(index)}>
-                {item.name}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {selectedServices.length > 0 && (
-        <div className="flex flex-col gap-3">
-          <h3 className="text-lg font-bold">Service</h3>
-          <ul className="flex flex-col gap-2">
-            {selectedServices.map((service, index) => (
-              <li key={index} className={`flex items-baseline gap-1 cursor-pointer m-0`}>
-                <input
-                  id={`checkbox-service-${index}`}
-                  type="checkbox"
-                  value={service.name}
-                  checked={selectedServiceOptions.includes(service.name)}
-                  onChange={() => handleServiceChange(service)}
-                  className="mr-2 cursor-pointer py-1"
-                />
-                <div
-                  className="flex justify-between cursor-pointer w-full gap-1"
-                  onClick={() => handleServiceChange(service)}
-                >
-                  <span className="">{service.name}</span>
-                  <div>
-                    <span className="bg-blue-500 text-white rounded-lg px-2">{service.count}</span>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  )
-}
-
 const TablePanel = ({
   selectedServices,
   handleAddServices,
@@ -226,7 +118,7 @@ const TablePanel = ({
         <span className="font-bold">Showing {selectedServices.length} Services</span>
         <span className="font-bold">Updated at 5:21:11 PM</span>
       </div>
-      <div className="overflow-x-auto scrollbar-table">
+      <div className="overflow-auto scrollbar-table">
         <table className="w-full table-auto text-white">
           <thead>
             <tr>
