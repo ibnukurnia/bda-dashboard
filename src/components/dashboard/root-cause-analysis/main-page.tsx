d5bceabe09bb6b154d2b6f59f5d9c98abf8b4939
Misbach <misbach.konsultan@work.bri.co.id>
2024-09-13 09:14:28 +0700

    smoothing  feature fullscreen

--- src/components/dashboard/root-cause-analysis/main-page.tsx
+++ src/components/dashboard/root-cause-analysis/main-page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './main-page.css'
 
@@ -14,7 +14,9 @@ import { getTimeDifference } from './helper'
 import { GetRootCauseAnalysisTree } from '@/modules/usecases/root-cause-analysis'
 import { RootCauseAnalysisTreeResponse } from '@/modules/models/root-cause-analysis'
 import useInterval from '@/hooks/use-interval'
-import FullscreenComponent from '../FullScreenComponent'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
+import Button from '@/components/system/Button/Button'
+import { Maximize } from 'react-feather'
 
 const defaultTimeRanges: Record<string, number> = {
   'Last 5 minutes': 5,
@@ -50,8 +52,7 @@ const MainPageRootCauseAnalysis = () => {
   const [initialLoading, setInitialLoading] = useState(true)
   const [modalServices, setModalServices] = useState(false)
 
-  const [isFullscreen, setIsFullscreen] = useState(false)
-  const componentToFullscreenedRef = useRef<HTMLDivElement>(null)
+  const handle = useFullScreenHandle();
 
   useEffect(() => {
     fetchData()
@@ -123,7 +124,7 @@ const MainPageRootCauseAnalysis = () => {
 
   return (
     <>
-      <div className={`flex flex-col gap-8 ${isFullscreen ? "py-8 px-6" : ""}`} ref={componentToFullscreenedRef}>
+      <div className='flex flex-col gap-8'>
         <div className='flex flex-row gap-2 self-end items-center'>
             <div className="flex flex-row gap-2 self-end items-center">
                 <Typography variant="body2" component="p" color="white">
@@ -136,24 +137,25 @@ const MainPageRootCauseAnalysis = () => {
                 />
             </div>
             <AutoRefreshButton onRefresh={fetchData} onAutoRefreshChange={handleAutoRefreshChange} />
-            <FullscreenComponent
-              elementRef={componentToFullscreenedRef}
-              onToggle={(isFullscreen) => setIsFullscreen(isFullscreen)}
-            />
+            <Button onClick={handle.enter}>
+                <Maximize className='w-6 h-5'/>
+            </Button>
         </div>
-        <div className="flex flex-col gap-10 px-2 py-8 card-style">
-          <div className="w-full flex flex-col gap-8">
-            {initialLoading ?
-              <div className="flex justify-center items-center">
-                  <div className="spinner"></div>
-              </div>
-              : <RCATree
-                data={dataTree}
-                handleDetail={handleDetail}
-              />
-            }
+        <FullScreen handle={handle}>
+          <div className={`flex flex-col gap-10 px-2 py-8 card-style ${handle.active ? "my-8 mx-6" : ""}`}>
+            <div className="w-full flex flex-col gap-8">
+              {initialLoading ?
+                <div className="flex justify-center items-center">
+                    <div className="spinner"></div>
+                </div>
+                : <RCATree
+                  data={dataTree}
+                  handleDetail={handleDetail}
+                />
+              }
+            </div>
           </div>
-        </div>
+        </FullScreen>
       </div>
       {modalServices && (
         <TableModal
