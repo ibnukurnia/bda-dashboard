c3e0a31b07bbec398ebf2a5a6050e1769938a47a
ask.websync <rafikarim@yahoo.com>
2024-10-01 16:37:11 +0700

    merge

--- src/components/dashboard/anomaly/main-page.tsx
+++ src/components/dashboard/anomaly/main-page.tsx
@@ -1,14 +1,9 @@
 // pages/tabs-page.tsx
 'use client'
 
-import { SetStateAction, useContext, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 // import { Box, Stack, Typography } from '@mui/material'
 import './main-page.css'
-import TabLogContent from './panels/log-panel'
-import TabUtilizationContent from './panels/utilization.panel'
-import TabNetworkContent from './panels/network-panel'
-import TabSecurityContent from './panels/security-panel'
-import { OverviewContext } from '@/contexts/overview-context'
 import { useSearchParams } from 'next/navigation';
 import TabsWithDropdown from './button/tabs-with-dropdown';
 import { DEFAULT_DATA_SOURCE_NAMESPACE, DEFAULT_TIME_RANGE } from '@/constants';
@@ -32,47 +27,6 @@ const MainPageAnomaly = () => {
   const searchParams = useSearchParams()
   const dataSource = searchParams.get("data_source") ?? DEFAULT_DATA_SOURCE_NAMESPACE
   const timeRange = searchParams.get("time_range") ?? DEFAULT_TIME_RANGE
-
-  const handleTabClick = (tab: string) => {
-    if (tab === 'log') {
-      setIsDropdownOpenUtilization(false);
-      setIsDropdownOpenSecurity(false);
-      setIsDropdownOpenLog(!isDropdownOpenLog); // Toggle Log dropdown
-    } else if (tab === 'utilization') {
-      setIsDropdownOpenLog(false);
-      setIsDropdownOpenSecurity(false);
-      setIsDropdownOpenUtilization(!isDropdownOpenUtilization); // Toggle Utilization dropdown
-    } else if (tab === 'network') {
-      setIsDropdownOpenLog(false);
-      setIsDropdownOpenUtilization(false);
-      setIsDropdownOpenSecurity(false);
-      setActiveTab('network');
-    } else if (tab === 'security') {
-      setIsDropdownOpenLog(false);
-      setIsDropdownOpenUtilization(false);
-      setIsDropdownOpenSecurity(!isDropdownOpenSecurity); // Toggle Security dropdown
-    }
-  };
-
-  const handleDropdownSelection = (type: string, value: SetStateAction<string>) => {
-    if (type === 'log') {
-      setSelectedLog(value);
-      setActiveTab('log'); // Only set Log tab active when a log is selected
-      setSelectedUtilization('');
-      setIsDropdownOpenLog(false);
-    } else if (type === 'utilization') {
-      setSelectedUtilization(value);
-      setActiveTab('utilization'); // Only set Utilization tab active when an option is selected
-      setSelectedLog('');
-      setIsDropdownOpenUtilization(false);
-    } else if (type === 'security') {
-      setSelectedSecurity(value);
-      setActiveTab('security'); // Only set Security tab active when an option is selected
-      setSelectedLog('');
-      setSelectedUtilization('');
-      setIsDropdownOpenSecurity(false);
-    }
-  };
 
   useEffect(() => {
     setActiveTab('log'); // Set log as the active tab initially
