3b89794151588625749143d450fef46e7aaf8370
Misbach <misbach.konsultan@work.bri.co.id>
2024-10-04 15:01:53 +0700

    ganti logo login

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
-// import RouterLink from 'next/link';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import Image from 'next/image';
-
-// import { paths } from '@/paths';
-// import { DynamicLogo } from '@/components/core/logo';
 
 export interface LayoutProps {
   children: React.ReactNode;
@@ -37,31 +32,22 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
       <Box
         sx={{
           alignItems: 'center',
-          // background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
           color: 'var(--mui-palette-common-white)',
           display: { xs: 'none', lg: 'flex' },
           justifyContent: 'center',
           p: 3,
         }}
       >
-        <Stack spacing={3}>
-          <Stack spacing={1}>
-            <img
-              src="/assets/logo-ops-vision.svg"
-              alt="OpsVision"
-              width="114.14px"
-              height="108.61px"
-            />
-            <div className='flex flex-col gap-3'>
-              <Typography color="white" fontWeight={700} fontSize={40} lineHeight={'48.76px'}>
-                OpsVision
-              </Typography>
-              <Typography color="white" fontWeight={600} fontSize={20} lineHeight={'24.38px'} sx={{ opacity: 0.8 }}>
-                Clear Insight for Confident Operation
-              </Typography>
-
-            </div>
-          </Stack>
+        <Stack className='w-max[565px] gap-5'>
+          <img
+            src="/assets/logo-ops-vision.svg"
+            alt="OpsVision"
+            width="355px"
+            height="73px"
+          />
+          <Typography color="white" fontWeight={600} fontSize={20} lineHeight={'24.38px'} sx={{ opacity: 0.8 }}>
+            Clear Insight for Confident Operation
+          </Typography>
         </Stack>
       </Box>
       <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column', background: 'rgba(255,255,255,0.1)' }}>
