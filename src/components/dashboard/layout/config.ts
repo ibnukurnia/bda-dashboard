9578cb85f90a808a6398337c36715210700c4438
Misbach <misbach.konsultan@work.bri.co.id>
2024-10-16 09:08:57 +0700

    title side nav menu

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -9,7 +9,7 @@ export const navItems: NavItemConfig[] = [
   { key: 'anomaly-detection', title: 'Anomaly Detection', href: paths.dashboard.anomaly, icon: 'menu-anomaly-detection' },
   // { key: 'situation', href: paths.dashboard.situation, icon: 'database' },
   { key: 'forecasting', title: 'Forecasting', href: paths.dashboard.forecasting, icon: 'menu-forecasting' },
-  { key: 'anomaly-notification', title: 'Notification', href: paths.dashboard.anomalyNotification, icon: 'bell' },
+  { key: 'anomaly-notification', title: 'Anomaly Notification', href: paths.dashboard.anomalyNotification, icon: 'bell' },
   { key: 'users-management', title: 'User Management', href: paths.dashboard.userManagement, icon: 'menu-user-management' },
   // { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
   // { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
