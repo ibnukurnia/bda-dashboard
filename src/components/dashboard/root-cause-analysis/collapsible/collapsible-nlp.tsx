4d2ac92b0bc33bb5521cce0a423c03beb6f54d12
Misbach <misbach.konsultan@work.bri.co.id>
2024-11-19 15:39:33 +0700

    feature: search incident - rca

--- src/components/dashboard/root-cause-analysis/collapsible/collapsible-nlp.tsx
+++ src/components/dashboard/root-cause-analysis/collapsible/collapsible-nlp.tsx
@@ -9,6 +9,7 @@ interface CollapsibleNLPProps {
   data: NLP; // Data to display in the collapsible section
   badge?: 'most_related' | 'additional' | null; // Badge type to highlight the section
   isOpen?: boolean; // Initial open state
+  showBadge?: boolean; // Initial show badge
 }
 
 const CollapsibleNLP = ({
@@ -16,6 +17,7 @@ const CollapsibleNLP = ({
   data,
   badge,
   isOpen = true, // Default open state is true
+  showBadge,
 }: CollapsibleNLPProps) => {
   const [open, setOpen] = useState(isOpen); // State to toggle the collapsible section
 
@@ -62,7 +64,7 @@ const CollapsibleNLP = ({
                 clipRule="evenodd"
                 d="M5 11C5 11.5523 5.44772 12 6 12C6.55228 12 7 11.5523 7 11V7H11C11.5523 7 12 6.55228 12 6C12 5.44772 11.5523 5 11 5H7V1C7 0.447715 6.55229 0 6 0C5.44772 0 5 0.447715 5 1V5H1C0.447715 5 0 5.44772 0 6C0 6.55228 0.447715 7 1 7H5V11Z"
                 fill="white"
-                fill-opacity="0.5"
+                fillOpacity="0.5"
               />
             </svg>
           )}
@@ -78,7 +80,7 @@ const CollapsibleNLP = ({
           {title}
         </Typography>
         {/* Badge Display */}
-        {badge && (
+        {showBadge && badge && (
           <div
             className="py-[3px] px-[13px] flex items-center justify-center border rounded-[23px]"
             style={{
